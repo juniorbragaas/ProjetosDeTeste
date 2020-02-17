02d5651e6e39b5b6c6247ab1f78cb15529b0eeeb
juniorbragaas <58236688+juniorbragaas@users.noreply.github.com>
2020-02-17 14:18:08 -0300

    1 projeto de teste

---
+++ ConexaoMySQL/ConexaoMySQL/Program.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConexaoMySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui você substitui pelos seus dados
+            string servidor = "";
+            string banco = "";
+            string usuario = "";
+            string senha = "";
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("conexao.xml"); 
+            XmlNodeList xnList = xmlDoc.GetElementsByTagName("conexao");
+
+            for (int i = 0; i < xnList.Count; i++)
+            {
+                servidor= xnList[i]["servidor"].InnerText;
+                banco = xnList[i]["banco"].InnerText;
+                usuario = xnList[i]["usuario"].InnerText;
+                senha= xnList[i]["senha"].InnerText;
+            }
+
+            var connString = "Server="+servidor+";Database="+banco+";Uid="+usuario+";Pwd="+senha;
+            var connection = new MySqlConnection(connString);
+            var command = connection.CreateCommand();
+
+            try
+            {
+                connection.Open();
+                command.CommandText = "SELECT * FROM pessoa";
+                command.ExecuteNonQuery();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine( reader["Nome"].ToString());
+                }
+
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+    }
+}
---
+++ ConsumnindoAPIConsole/ConsumnindoAPIConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsumnindoAPIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite o CEP: ");
+            var valor = System.Console.ReadLine();
+            try
+            {
+                var ws = new WSCorreios.AtendeClienteClient();
+                var resposta = ws.consultaCEPAsync(valor);
+                System.Console.WriteLine();
+                System.Console.WriteLine("Endereço: {0}", resposta.Result.@return.end);
+                System.Console.WriteLine("Complemento: {0}", resposta.Result.@return.complemento2);
+                System.Console.WriteLine("Bairro: {0}", resposta.Result.@return.bairro);
+                System.Console.WriteLine("Cidade: {0}", resposta.Result.@return.cidade);
+                System.Console.WriteLine("Estado: {0}", resposta.Result.@return.uf);
+                System.Console.WriteLine("Unidades de Postagem: {0}", resposta.Result.@return.unidadesPostagem);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Erro ao efetuar busca do CEP: {0}", ex.Message);
+            }
+            System.Console.ReadLine();
+        }
+    }
+}

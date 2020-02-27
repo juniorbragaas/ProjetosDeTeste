415f33a108b85f5add94fd1a893a30a51bb4f842
Valdelei Junior Braga <junior.braga,as@gmail.com>
2020-02-27 00:40:02 -0300

    APICrudSQLNETCore
    Primeira Teste com banco

---
+++ ApiCrud/ApiCrud/Models/APIContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ApiCrud.Models
+{
+    public partial class APIContext : DbContext
+    {
+        public APIContext()
+        {
+        }
+
+        public APIContext(DbContextOptions<APIContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Login)
+                    .IsRequired()
+                    .HasColumnName("login")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ApiCrud/ApiCrud/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiCrud.Models
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}

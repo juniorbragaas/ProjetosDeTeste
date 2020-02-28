d0453938e9885a5df01a1e73c5551d0c542353af
Valdelei Junior Braga <junior.braga,as@gmail.com>
2020-02-27 21:21:43 -0300

    fdsgdsf
    sfdgsdf

--- ApiCrud/ApiCrud/Controllers/WeatherForecastController.cs
+++ ApiCrud/ApiCrud/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace TodoApi.Controllers
+namespace ApiCrud.Controllers
 {
     [ApiController]
     [Route("[controller]")]

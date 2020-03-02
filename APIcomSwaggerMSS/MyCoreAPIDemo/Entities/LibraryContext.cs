c1c5021f191b452bae630421cc667ff18277e748
juniorbragaas <58236688+juniorbragaas@users.noreply.github.com>
2020-03-02 16:35:28 -0300

    APIswaggerMSSS
    APIswaggerMSSS

---
+++ APIcomSwaggerMSS/MyCoreAPIDemo/Entities/LibraryContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCoreAPIDemo.Entities
+{
+    public class LibraryContext:DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Users> Users { get; set; }
+
+
+        
+
+    }
+}
